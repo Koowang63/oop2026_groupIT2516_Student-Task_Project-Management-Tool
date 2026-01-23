44b8563508ff09053ba5f1d42e98a206bf9d8bf9
Bainazar <baitatop273@gmail.com>
2026-01-24 02:18:05 +0500

    Initial commit from IntelliJ

--- src/edu/aitu/oop3/db/DatabaseConnection.java
+++ src/edu/aitu/oop3/db/DatabaseConnection.java
@@ -5,8 +5,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 public class DatabaseConnection {
     private static final String URL =
-            "jdbc:postgresql://aws-1-ap-northeast-1.pooler.supabase.com:5432/postgres?sslmode=require";
-    private static final String USER = "postgres.mrivkaqomaorywhzsntk";
+            "jdbc:postgresql://db.xouujdmgcoflekqlgqwm.supabase.co:5432/postgres?sslmode=require";
+    private static final String USER = "postgres";
     private static final String PASSWORD = "bainazarproject";// ← DATABASE PASSWORD
 
     private DatabaseConnection() {
