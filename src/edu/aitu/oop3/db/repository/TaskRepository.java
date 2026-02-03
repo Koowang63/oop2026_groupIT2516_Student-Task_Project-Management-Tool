42fd707882c10e58e139407bd7dd2647e66c03be
Bainazar <baitatop273@gmail.com>
2026-02-03 18:26:32 +0500

    Mekembay Nurtas, updating the code TaskRepository

--- src/edu/aitu/oop3/db/repository/TaskRepository.java
+++ src/edu/aitu/oop3/db/repository/TaskRepository.java
@@ -2,16 +2,6 @@ package edu.aitu.oop3.db.repository;
 
 import edu.aitu.oop3.db.entity.Task;
 
-import java.util.List;
-import java.util.Optional;
-
-public interface TaskRepository {
-
-    Task save(Task task);
-
-    Optional<Task> findById(Long id);
-
-    List<Task> findAll();
-
+public interface TaskRepository extends Repository<Task, Long> {
     void updateStatus(Long taskId, String status);
 }
