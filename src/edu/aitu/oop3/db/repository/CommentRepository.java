7932748ba3c1338bc70d88c68146196b6e55df1d
Bainazar <baitatop273@gmail.com>
2026-02-03 18:26:59 +0500

    Mekembay Nurtas, updating the code CommentRepository

--- src/edu/aitu/oop3/db/repository/CommentRepository.java
+++ src/edu/aitu/oop3/db/repository/CommentRepository.java
@@ -3,7 +3,6 @@ package edu.aitu.oop3.db.repository;
 import edu.aitu.oop3.db.entity.Comment;
 import java.util.List;
 
-public interface CommentRepository {
-    Comment save(Comment comment);
+public interface CommentRepository extends Repository<Comment, Long> {
     List<Comment> findByTaskId(Long taskId);
 }
