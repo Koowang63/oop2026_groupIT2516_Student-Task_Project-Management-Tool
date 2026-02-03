1e589a633ba5dd44645d074a2ad4351c4f7cef8c
Bainazar <baitatop273@gmail.com>
2026-02-03 18:25:15 +0500

    Mekembay Nurtas, creating Repository and updating the code UserRepository

---
+++ src/edu/aitu/oop3/db/repository/Repository.java
@@ -0,0 +1,10 @@
+package edu.aitu.oop3.db.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Repository<T, ID> {
+    T save(T entity);
+    Optional<T> findById(ID id);
+    List<T> findAll();
+}
--- src/edu/aitu/oop3/db/repository/UserRepository.java
+++ src/edu/aitu/oop3/db/repository/UserRepository.java
@@ -1,11 +1,6 @@
 package edu.aitu.oop3.db.repository;
 
 import edu.aitu.oop3.db.entity.User;
-import java.util.List;
-import java.util.Optional;
 
-public interface UserRepository {
-    User save(User user);
-    Optional<User> findById(Long id);
-    List<User> findAll();
+public interface UserRepository extends Repository<User, Long> {
 }
