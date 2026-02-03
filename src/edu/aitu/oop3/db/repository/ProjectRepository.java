ed3c5c4494b0e5383aab6acce96f2e8a040681b7
Bainazar <baitatop273@gmail.com>
2026-02-03 18:26:03 +0500

    Mekembay Nurtas, updating the code ProjectRepository

--- src/edu/aitu/oop3/db/repository/ProjectRepository.java
+++ src/edu/aitu/oop3/db/repository/ProjectRepository.java
@@ -1,11 +1,6 @@
 package edu.aitu.oop3.db.repository;
 
 import edu.aitu.oop3.db.entity.Project;
-import java.util.List;
-import java.util.Optional;
 
-public interface ProjectRepository {
-    Project save(Project project);
-    Optional<Project> findById(Long id);
-    List<Project> findAll();
+public interface ProjectRepository extends Repository<Project, Long> {
 }
